0cb3cdd6408e4eb798c8886c18e6008ea05990c4
akilasachitra <akilasachitra@gmail.com>
2021-12-19 16:46:15 +0530

    revision2.2

---
+++ src/main/java/com/hmps/hmps/general/PatientDetail.java
@@ -0,0 +1,138 @@
+package com.hmps.hmps.general;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.*;
+
+public class PatientDetail {
+
+    @javafx.fxml.FXML
+    private Button cancel;
+    @javafx.fxml.FXML
+    private TextField textLName;
+    @javafx.fxml.FXML
+    private TextField textSurName;
+    @javafx.fxml.FXML
+    private ComboBox cmbBoxMaritalStatus;
+    @javafx.fxml.FXML
+    private Button save;
+    @javafx.fxml.FXML
+    private DatePicker dpBirthDate;
+    @javafx.fxml.FXML
+    private TextField textHISCode;
+    @javafx.fxml.FXML
+    private ComboBox cmbBoxNationality;
+    @javafx.fxml.FXML
+    private TextField textFax;
+    @javafx.fxml.FXML
+    private ComboBox cmbBoxCountry;
+    @javafx.fxml.FXML
+    private TextField textFName;
+    @javafx.fxml.FXML
+    private TextField textAddress3;
+    @javafx.fxml.FXML
+    private ComboBox cmbBoxLanguage;
+    @javafx.fxml.FXML
+    private TextField textTp2;
+    @javafx.fxml.FXML
+    private TextField textAddress2;
+    @javafx.fxml.FXML
+    private TextField textTp1;
+    @javafx.fxml.FXML
+    private TextField textAddress1;
+    @javafx.fxml.FXML
+    private TextField textEmail;
+    @javafx.fxml.FXML
+    private TextField textSocialSecurityCode;
+    @javafx.fxml.FXML
+    private ComboBox cmbBoxGender;
+    @javafx.fxml.FXML
+    private TextField textCode;
+    @javafx.fxml.FXML
+    private ComboBox cmbBoxBloodGroup;
+    @javafx.fxml.FXML
+    private Button newData;
+    @javafx.fxml.FXML
+    private CheckBox btnSearchByMaritalStatus;
+    @javafx.fxml.FXML
+    private CheckBox btnSearchByLanguage;
+    @javafx.fxml.FXML
+    private ComboBox cmbBoxSerchByGender;
+    @javafx.fxml.FXML
+    private CheckBox btnSearchByGender;
+    @javafx.fxml.FXML
+    private TextField textSearchLName;
+    @javafx.fxml.FXML
+    private TextField textMaidenName;
+    @javafx.fxml.FXML
+    private RadioButton btnSearchByAge;
+    @javafx.fxml.FXML
+    private RadioButton btnSearchByHISCode;
+    @javafx.fxml.FXML
+    private TextField textSearchMaidenName;
+    @javafx.fxml.FXML
+    private RadioButton btnSearchBySSCode;
+    @javafx.fxml.FXML
+    private TextField textSearchCode;
+    @javafx.fxml.FXML
+    private TextField textSearchAgeTo;
+    @javafx.fxml.FXML
+    private Button search;
+    @javafx.fxml.FXML
+    private RadioButton btnSearchByCode;
+    @javafx.fxml.FXML
+    private ComboBox cmbBoxSerchByBloodGroup;
+    @javafx.fxml.FXML
+    private TextField textSearchHISCode;
+    @javafx.fxml.FXML
+    private Button btnUpload;
+    @javafx.fxml.FXML
+    private TextField textSearchSSCode;
+    @javafx.fxml.FXML
+    private CheckBox btnSearchByBloodGroup;
+    @javafx.fxml.FXML
+    private ComboBox cmbBoxSerchByCountry;
+    @javafx.fxml.FXML
+    private TextField textSearchAgeFrom;
+    @javafx.fxml.FXML
+    private TextField textSearchSurName;
+    @javafx.fxml.FXML
+    private TextField textSearchFName;
+    @javafx.fxml.FXML
+    private CheckBox btnSearchByCountry;
+    @javafx.fxml.FXML
+    private ComboBox cmbBoxSerchByMaritalStatus;
+    @javafx.fxml.FXML
+    private ComboBox cmbBoxSerchByLanguage;
+
+    @javafx.fxml.FXML
+    public void onSave(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void onCancel(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void onNew(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void onBtnSearchByCode(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void onBtnSearchByHISCode(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void onUpload(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void onSearch(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void onBtnSearchBySSCode(ActionEvent actionEvent) {
+    }
+}
